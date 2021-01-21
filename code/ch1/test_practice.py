2882c60c4288a9965e0055097d631e76df5af1a6
t-yuki-2021 <yuki.tanio0628@gmail.com>
2021-01-21 14:55:09 +0900

    CHAPTER1練習問題

---
+++ code/ch1/test_practice.py
@@ -0,0 +1,13 @@
+def test_pass():
+    a = "test"
+    assert "test" == a
+
+def test_pass_list():
+    l = [1, 2, 3]
+    assert 1 in l
+
+def test_pass_atob():
+    assert 'a' < 'b' 
+
+def test_pass_fizzbazz():
+    assert 'fizz' in 'fizzbazz' 
